a9ad28ad845295532dab5531b112b28e8757b90c
Pureliux <amirtirana.postfach@gmail.com>
2026-05-18 08:58:59 +0200

    Refine login background geometry

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 
-const APP_VERSION = '2026-05-18-domain-db-polish-v10';
+const APP_VERSION = '2026-05-18-login-hexagon-bg-v11';
 
 async function refreshIfAppShellIsStale() {
 	if (!('fetch' in window)) {
